ab0b22f401e4c7cacaaf262915ae90bccb1efaab
tize1 <hermanotize@gmail.com>
2025-12-26 15:10:14 +0100

    mail prob corrected

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 import BoardsPage from "./pages/BoardsPage";
 import BoardPage from "./pages/BoardPage";
-//import Navbar from "./components/common/Navbar";
+import Navbar from "./components/common/Navbar";
 import { useAuth } from "./context/AuthContext";
 
 export default function App() {
-  const { user } = useAuth();
+  const { user   } = useAuth();
 
   return (
     <BrowserRouter>
-      {user }
+      {user && Navbar}
       <Routes>
         <Route path="/" element={user ? <Navigate to="/boards" /> : <Login />} />
         <Route path="/signup" element={<Signup />} />
